510309d707355e6111fa75ea5a225c0ade761454
Kelyn Wong <kelynwonget@gmail.com>
2024-09-16 09:10:25 +0000

    change if condition

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ login_page = st.Page("login.py", title="Login", icon=":material/login:")
 workout_page = st.Page("workout.py", title="Workout Dashboard", icon=":material/fitness_center:")
 
 # Navigation for the app
-if "authenticated" in st.session_state and st.session_state.get("authenticated"):
+if st.session_state.get("authenticated"):
     # If the user is authenticated, make both login and workout pages available
     pg = st.navigation([workout_page])
 else:
